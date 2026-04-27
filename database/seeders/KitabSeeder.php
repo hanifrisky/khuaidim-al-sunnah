da10848512834fd5be32a6f558915edff21be88d
Hanif Samsu <hanifsamsuanggarisky@gmail.com>
2026-04-27 11:08:58 +0700

    update cover

--- database/seeders/KitabSeeder.php
+++ database/seeders/KitabSeeder.php
@@ -14,37 +14,37 @@ class KitabSeeder extends Seeder
                 'name' => 'صحيح البخاري',
                 'author' => 'Sahih Al-bukhari',
                 'description' => '-',
-                'media' => 'kitabs/cover/1.jpg',
+                'media' => 'cover/1.jpg',
             ],
             [
                 'name' => 'صحيح مسلم',
                 'author' => 'Sahih Muslim',
                 'description' => '-',
-                'media' => 'kitabs/cover/2.jpg',
+                'media' => 'cover/2.jpg',
             ],
             [
                 'name' => 'سنن ابي داود',
                 'author' => 'Sunan Abu Dawud',
                 'description' => '-',
-                'media' => 'kitabs/cover/3.jpg',
+                'media' => 'cover/3.jpg',
             ],
             [
                 'name' => 'سنن الترمذي',
                 'author' => 'Jami at-Tirmidzi',
                 'description' => '-',
-                'media' => 'kitabs/cover/4.jpg',
+                'media' => 'cover/4.jpg',
             ],
             [
                 'name' => 'سنن النسائي',
                 'author' => 'Sunan an-Nasa\'i',
                 'description' => '-',
-                'media' => 'kitabs/cover/5.jpg',
+                'media' => 'cover/5.jpg',
             ],
             [
                 'name' => 'سنن ابن ماجه',
                 'author' => 'Sunan Ibnu Majah',
                 'description' => '-',
-                'media' => 'kitabs/cover/6.jpg',
+                'media' => 'cover/6.jpg',
             ],
         ];
         foreach ($kitabs as $kitab) {
