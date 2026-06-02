b9ac8b6df35dbd24e2c9908e3fef6e33d3ff5906
Hanif Samsu <hanifsamsuanggarisky@gmail.com>
2026-06-02 14:32:53 +0700

    Add Favicon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Filament\Facades\Filament;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +21,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Filament::registerRenderHook(
+            'panels::head.end',
+            fn(): string => '
+                <link rel="apple-touch-icon" sizes="180x180" href="' . asset('apple-touch-icon.png') . '">
+                <link rel="icon" type="image/png" sizes="32x32" href="' . asset('favicon-32x32.png') . '">
+                <link rel="icon" type="image/png" sizes="16x16" href="' . asset('favicon-16x16.png') . '">
+                <link rel="manifest" href="' . asset('site.webmanifest') . '">
+            '
+        );
     }
 }
